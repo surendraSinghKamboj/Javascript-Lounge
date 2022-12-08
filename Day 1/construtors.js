d0cc5c293beaa192cd54ded35d1ebf657ba74a46
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2022-12-08 14:34:50 +0530

    Create construtors.js

---
+++ Day 1/construtors.js
@@ -0,0 +1,20 @@
+console.clear();
+
+const str = new String(""); // A new String object
+const num = new Number(9826); // A new Number object
+const bool = new Boolean(true); // A new Boolean object
+const obj = new Object({name:"Sunnu"}); // A new Object object
+const arr = new Array([1,2,3,4]); // A new Array object
+const regx = new RegExp(); // A new RegExp object
+const func = new Function(); // A new Function object
+const dat = new Date(); // A new Date object
+
+
+console.log(str)
+console.log(num)
+console.log(bool)
+console.log(obj)
+console.log(arr)
+console.log(regx)
+console.log(func)
+console.log(dat)
